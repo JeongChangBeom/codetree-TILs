7b4f4a4b7b451a4d4e08ecc10e5e2b5c4bbabcef
ChangBeom <87409157+JeongChangBeom@users.noreply.github.com>
2024-04-08 17:27:23 +0900

    Title: 변수 값 교체하기 6, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240408/변수 값 교체하기 6/replacing-variable-values-6.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a = 2;
+    int b = 5;
+    int temp;
+    temp = b;
+    b = a;
+    a = temp;
+
+    cout << a << '\n' << b;
+    return 0;
+}

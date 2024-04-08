bef270ba816c3c85eca9ce73d434d6bb0bbc8461
ChangBeom <87409157+JeongChangBeom@users.noreply.github.com>
2024-04-08 17:19:07 +0900

    Title: 두 실수의 곱, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240408/두 실수의 곱/the-product-of-two-real-numbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << fixed;
+    cout.precision(3);
+
+    float a = 5.26;
+    float b = 8.27;
+
+    cout << a*b;
+
+    return 0;
+}

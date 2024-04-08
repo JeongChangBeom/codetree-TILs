36198516befee72d736ffcf998dbcc0528512e1e
ChangBeom <87409157+JeongChangBeom@users.noreply.github.com>
2024-04-08 19:06:52 +0900

    Title: 자연수면 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240408/자연수면 출력/output-only-natural-number.cpp
+++ 240408/자연수면 출력/output-only-natural-number.cpp
@@ -5,10 +5,13 @@ using namespace std;
 int main() {
     int a,b;
     cin >> a >> b;
-    if(a>=0){
+    if(a>0){
         for(int i=0;i<b;i++){
             cout << a;
         }
+    }
+    else{
+        cout << 0;
     }
     return 0;
 }

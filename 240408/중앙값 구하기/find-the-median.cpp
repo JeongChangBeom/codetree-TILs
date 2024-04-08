bf90ad67da36aac8024022a6a799be11cc3b4775
ChangBeom <87409157+JeongChangBeom@users.noreply.github.com>
2024-04-08 18:53:54 +0900

    Title: 중앙값 구하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240408/중앙값 구하기/find-the-median.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int main() {
+    int a,b,c;
+    cin >> a >> b >>c;
+
+    vector<int> v;
+    v.push_back(a);
+    v.push_back(b);
+    v.push_back(c);
+
+    sort(v.begin(),v.end());
+
+    cout << v[1];
+    return 0;
+}

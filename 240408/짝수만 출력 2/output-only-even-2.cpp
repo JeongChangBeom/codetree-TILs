016f103c6a654fc4bb66c0ce7ef0847bc83a2ded
ChangBeom <87409157+JeongChangBeom@users.noreply.github.com>
2024-04-08 19:02:52 +0900

    Title: 짝수만 출력 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240408/짝수만 출력 2/output-only-even-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a,b;
+    cin a >> b;
+
+    while(a<=b){
+        if(a%2==0){
+            cout << a << ' ';
+        }
+        a--;
+    }
+    return 0;
+}

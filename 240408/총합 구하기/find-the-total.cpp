a7a7e654ed008b1e16b13c5d7a322e1d29ff437f
ChangBeom <87409157+JeongChangBeom@users.noreply.github.com>
2024-04-08 19:11:42 +0900

    Title: 총합 구하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240408/총합 구하기/find-the-total.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    int a,b;
+    cin >> a >> b;
+
+    int result = 0;
+    for(int i=a;i<=b;i++){
+        if(i%6==0 && i%8!=0){
+            result += i;
+        }
+    }
+    cout << result;
+    return 0;
+}

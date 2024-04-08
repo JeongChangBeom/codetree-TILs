158c4797bdd1b46181fc918838b6092545a7b029
ChangBeom <87409157+JeongChangBeom@users.noreply.github.com>
2024-04-08 18:59:36 +0900

    Title: 별 n개 출력하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240408/별 n개 출력하기/print-n-stars.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int start = 0;
+    while(start < n){
+        cout << '*' << '\n'
+        start++;
+    }
+    return 0;
+}

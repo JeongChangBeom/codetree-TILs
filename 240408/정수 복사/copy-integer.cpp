8c1b03d3e482232d5137aa8d432602aed1009c2c
ChangBeom <87409157+JeongChangBeom@users.noreply.github.com>
2024-04-08 17:25:00 +0900

    Title: 정수 복사, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240408/정수 복사/copy-integer.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a = 3;
+    int b = 4;
+
+    int b = a;
+
+    cout << a << " " << b << '\n';
+    cout << a*b;
+    return 0;
+}

a2e5387d5de22c141b043f161b5ce115bc2c73a3
ChangBeom <87409157+JeongChangBeom@users.noreply.github.com>
2024-04-08 18:50:33 +0900

    Title: 두 사람, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240408/두 사람/two-person.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    bool check = false;
+
+    for(int i=0;i<2;i++){
+        int age;
+        char sex;
+        cin >> age >> sex;
+
+        if(age >= 19 && sex == M){
+            check = true;
+        }
+    }
+
+    if(check){
+        cout << 1;
+    }
+    else{
+        cout << 0;
+    }
+
+    return 0;
+}

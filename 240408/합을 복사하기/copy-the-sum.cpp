0f93572cd53aa2a8e3669db83320e05559e89f70
ChangBeom <87409157+JeongChangBeom@users.noreply.github.com>
2024-04-08 17:34:53 +0900

    Title: 합을 복사하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240408/합을 복사하기/copy-the-sum.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    int a = 1;
+    int b = 2;
+    int c = 3;
+    int sum = a+b+c;
+    a = sum;
+    b = sum;
+    c = sum;
+    cout << a << ' ' << b << ' ' << c;
+    return 0;
+}

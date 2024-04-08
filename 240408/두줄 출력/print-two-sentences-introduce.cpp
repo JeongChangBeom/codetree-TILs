09bab0b752a3e07678f8cfc91e65f7977600de67
ChangBeom <87409157+JeongChangBeom@users.noreply.github.com>
2024-04-08 16:54:34 +0900

    Title: 두줄 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240408/두줄 출력/print-two-sentences-introduce.cpp
+++ 240408/두줄 출력/print-two-sentences-introduce.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() {
-    cout << "Hello students!" << '\n' << "Welcome to LeebrosCode!"
+    cout << "Hello students!" << '\n' << "Welcome to LeebrosCode!";
     return 0;
 }

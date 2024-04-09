50d0fb815b8b531c03f0e24ac7daceaea31c8a3d
ChangBeom <87409157+JeongChangBeom@users.noreply.github.com>
2024-04-09 16:50:33 +0900

    Title: 정사각형 출력, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240409/정사각형 출력/print-square.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for(int i=1;i<=n*n;i++){
+        cout << i << " ";
+        if(i%3==0){
+            cout << '\n';
+        }
+    }
+    return 0;
+}

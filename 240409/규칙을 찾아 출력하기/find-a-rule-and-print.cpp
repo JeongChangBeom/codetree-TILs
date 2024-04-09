8271a3d56f2ab0e5e83a81c8296d1226a3242989
ChangBeom <87409157+JeongChangBeom@users.noreply.github.com>
2024-04-09 16:16:51 +0900

    Title: 규칙을 찾아 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240409/규칙을 찾아 출력하기/find-a-rule-and-print.cpp
+++ 240409/규칙을 찾아 출력하기/find-a-rule-and-print.cpp
@@ -8,7 +8,7 @@ int main() {
 
     for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                if(i==0||i==n-1){
+                if(i==0||j==n-1){
                     cout << "*" << " ";
                 }
                 else if(i<=j){

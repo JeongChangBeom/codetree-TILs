10108e53360636c09a40398eee478a2bb819f060
ChangBeom <87409157+JeongChangBeom@users.noreply.github.com>
2024-04-09 19:05:05 +0900

    Title: 격자 반대로 채우기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240409/격자 반대로 채우기/grid-anti-fill.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int arr[10][10];
+
+int main() {
+    int n;
+    cin >> n;
+
+    int start = 1;
+
+    for(int i=n;i>0;i--){
+        if(i%2==0){
+            for(int j=n;j>0;j--){
+                arr[j][i] = start;
+                start++;
+            }
+        }
+        else{
+            for(int j=1;j<=n;j++){
+                arr[j][i] = start;
+                start++;
+            }
+        }
+    }
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            cout << arr[i][j] << " ";
+        }
+        cout << '\n';
+    }
+    return 0;
+}

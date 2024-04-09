51dc74e1f6004d9c17bcd72ca5dea335a8b7af57
ChangBeom <87409157+JeongChangBeom@users.noreply.github.com>
2024-04-09 15:40:43 +0900

    Title: 별표 출력하기 12, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240409/별표 출력하기 12/print-star-12.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for(int i=0;i<n;i++){
+        if(i==0){
+            for(int j=0;j<n;j++){
+                cout << "*" << " ";
+            }
+        }
+        else{
+            for(int j=0;j<n;j++){
+                if(j%2==0 && j>=i){
+                    cout << " " << " ";
+                }
+                else{
+                    cout << "*" << " ";
+                }
+            }
+        }
+    }
+    return 0;
+}

cbcb7613dcbd4af418cb398586ebc0bf66fdef88
ChangBeom <87409157+JeongChangBeom@users.noreply.github.com>
2024-04-09 17:50:17 +0900

    Title: 알파벳 출력하기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240409/알파벳 출력하기 2/print-alphabet-2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    char alpha = 'A';
+
+    for(int i=1;i<=N;i++){
+        for(int j=1;j<=N;j++){
+            if(i<=j){
+                cout << alpha << " ";
+                if(alpha == 'Z'){
+                    alpha == 'A';
+                }
+                alpha++;
+            }
+            else{
+                cout << " " << " ";
+            }
+        }
+        cout <<'\n';
+    }
+
+    return 0;
+}

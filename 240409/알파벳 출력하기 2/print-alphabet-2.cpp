95ab41c04054c959548426aaba93bbf2e635fd21
ChangBeom <87409157+JeongChangBeom@users.noreply.github.com>
2024-04-09 17:53:57 +0900

    Title: 알파벳 출력하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240409/알파벳 출력하기 2/print-alphabet-2.cpp
+++ 240409/알파벳 출력하기 2/print-alphabet-2.cpp
@@ -8,13 +8,13 @@ int main() {
 
     char alpha = 'A';
 
-    for(int i=1;i<=N;i++){
-        for(int j=1;j<=N;j++){
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++){
             if(i<=j){
                 cout << alpha << " ";
                 alpha++;
                 if(alpha > 'Z'){
-                    alpha == 'A';
+                    alpha = 'A';
                 }
             }
             else{
@@ -23,6 +23,5 @@ int main() {
         }
         cout <<'\n';
     }
-
     return 0;
 }

1a7e5d4e33e7872cfe62d6c6bb576e848d49421e
ChangBeom <87409157+JeongChangBeom@users.noreply.github.com>
2024-04-09 17:58:07 +0900

    Title: 짝수의 합 n번 구하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240409/짝수의 합 n번 구하기/find-the-sum-of-even-numbers-n-times.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int sum = 0;
+
+    for(int i=0;i<n;i++){
+        sum = 0;
+        int a,b;
+        cin >> a >> b;
+        for(int j=a;j<=b;j++){
+            if(j%2==0){
+                sum += j;
+            }
+        }
+        cout << sum; << '\n'
+    }
+    return 0;
+}

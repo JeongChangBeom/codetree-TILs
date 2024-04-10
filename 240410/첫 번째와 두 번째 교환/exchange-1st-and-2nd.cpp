e3ba19e044e62672c69ad41d460700941c1969c9
ChangBeom <87409157+JeongChangBeom@users.noreply.github.com>
2024-04-10 18:15:23 +0900

    Title: 첫 번째와 두 번째 교환, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240410/첫 번째와 두 번째 교환/exchange-1st-and-2nd.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include<string>
+
+using namespace std;
+
+int main() {
+    string str;
+    cin >> str;
+
+    char first = str[0];
+    char second = str[1];
+
+    for(int i=0;i<str.size();i++){
+        if(str[i] == first){
+            str[i] = second;
+        }
+        else if(str[i] == second){
+            str[i] = first;
+        }
+    }
+
+    cout << str;
+
+    return 0;
+}

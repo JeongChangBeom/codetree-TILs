8f77b8ddf27e1126072fe9222b2c23369a06d30e
ChangBeom <87409157+JeongChangBeom@users.noreply.github.com>
2024-04-11 23:51:32 +0900

    Title: 음수 구별하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240411/음수 구별하기/separate-negative-number.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    if(n<0){
+        cout << n << '\n' << "minus";
+    }
+    else{
+        cout << n;
+    }
+    return 0;
+}

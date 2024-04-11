79c85cff7b436bfbbe76123348c0dc0c0f8a769d
ChangBeom <87409157+JeongChangBeom@users.noreply.github.com>
2024-04-11 23:46:47 +0900

    Title: 미는 횟수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240411/미는 횟수/number-of-pushes.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include<string>
+#include<deque>
+
+using namespace std;
+
+bool compare(deque<char> x, deque<char> y) {
+    bool check = true;
+
+    for (int i = 0; i < x.size(); i++) {
+        if (x[i] != y[i]) {
+            check = false;
+        }
+    }
+    return check;
+}
+
+int main() {
+    string A, B;
+    cin >> A >> B;
+
+    deque<char> d_A;
+    deque<char> d_B;
+
+    for (int i = 0; i < A.size(); i++) {
+        d_A.push_back(A[i]);
+    }
+    for (int i = 0; i < B.size(); i++) {
+        d_B.push_back(B[i]);
+    }
+
+    int result = -1;
+
+    for (int i = 0; i < d_A.size(); i++) {
+        if (compare(d_A, d_B)) {
+            break;
+        }
+        else {
+            d_A.push_front(d_A.back());
+            d_A.pop_back();
+            result++;
+        }
+    }
+
+    if (result == -1) {
+        cout << result;
+    }
+    else {
+        cout << result + 1;
+    }
+
+    return 0;
+}

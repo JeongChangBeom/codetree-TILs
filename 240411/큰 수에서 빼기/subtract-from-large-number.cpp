287dea6e2c601909d537bc232a21db53254c8c54
ChangBeom <87409157+JeongChangBeom@users.noreply.github.com>
2024-04-11 23:52:30 +0900

    Title: 큰 수에서 빼기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240411/큰 수에서 빼기/subtract-from-large-number.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a,b;
+    cin >> a >> b;
+
+    if(a<b){
+        cout << b-a;
+    }
+    else{
+        cout << a-b;
+    }
+    return 0;
+}

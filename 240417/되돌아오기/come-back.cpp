7e005df76ed76e5bf11cea70020a11444256626d
ChangBeom <87409157+JeongChangBeom@users.noreply.github.com>
2024-04-17 15:56:31 +0900

    Title: 되돌아오기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240417/되돌아오기/come-back.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using namespace std;
+
+int dx[4] = {1,0,-1,0};
+int dy[4] = {0,1,0,-1};
+
+int main() {
+    int N;
+    cin >> N;
+
+    int x = 0;
+    int y = 0;
+    int rot_num;
+    int result = 0;
+
+    for(int i=0;i<N;i++){
+        char rot;
+        int dis;
+        cin >> rot >> dis;
+
+        if(rot == 'N'){
+            rot_num = 0;
+        }
+        else if(rot == 'E'){
+            rot_num = 1;
+        }
+        else if(rot == 'S'){
+            rot_num = 2;
+        }
+        else if(rot == 'W'){
+            rot_num = 3;
+        }
+
+        for(int j=0;j<dis;j++){
+            if(x==0 && y==0 && result!=0){
+                cout << result;
+                return 0;
+            }
+
+            x += dx[rot_num];
+            y += dy[rot_num];
+            result++;
+        }
+    }
+    cout << "-1";
+}

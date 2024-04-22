32e8d43037218333c3721fbc03298c77a2d12aca
ChangBeom <87409157+JeongChangBeom@users.noreply.github.com>
2024-04-22 17:03:51 +0900

    Title: 빙빙 돌며 사각형 채우기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240422/빙빙 돌며 사각형 채우기/snail-alphabet-square.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+int dx[4] = {0,1,0,-1};
+int dy[4] = {1,0,-1,0};
+char alpha[26] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N'
+                ,'O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+
+char arr[101][101];
+
+int main() {
+    int n,m;
+
+    cin >> n >> m;
+
+    int x=0;
+    int y=0;
+    int rot_num=0;
+
+    arr[x][y] = alpha[0];
+
+    for(int i=2;i<=n*m;i++){
+        int next_x = x + dx[rot_num];
+        int next_y = y + dy[rot_num];
+
+        if(!(next_x>=0 && next_x<n && next_y>=0 && next_y<m ) || arr[next_x][next_y] != 0){
+            rot_num = (rot_num+1)%4;
+        }
+
+        x += dx[rot_num];
+        y += dy[rot_num];
+
+        arr[x][y] = alpha[i-1%26];
+    }
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cout << arr[i][j] << " ";
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}

cea5269da89d6eed00ce43be2eb864ecf9e26f6d
ChangBeom <87409157+JeongChangBeom@users.noreply.github.com>
2024-04-28 15:22:58 +0900

    Title: 길이 변형 후 사각형 넓이 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240428/길이 변형 후 사각형 넓이 구하기/square-width-after-length-change.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int w,h;
+    cin >> w >> h;
+
+    cout << w+8 << '\n';
+    cout << h*3 << '\n';
+    cout << (w+8)*(h*3);
+
+    return 0;
+}

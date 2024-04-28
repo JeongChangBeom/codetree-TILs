15961d24785fd9ede73596315a624684b765a3de
ChangBeom <87409157+JeongChangBeom@users.noreply.github.com>
2024-04-28 15:50:37 +0900

    Title: 숫자의 조건 여부, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240428/숫자의 조건 여부/number's-condition.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+
+    if(a >= 113){
+        cout << 1;
+    }
+    else{
+        cout << 0;
+    }
+    return 0;
+}

bdbef3a7a9f19fea48e33acec6664d0e46ff7057
ChangBeom <87409157+JeongChangBeom@users.noreply.github.com>
2024-04-29 16:20:35 +0900

    Title: 비교 연산, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240429/비교 연산/comparison-operator.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a,b;
+    cin >> a >> b;
+    
+    if(a >= b){
+        cout << 1 << '\n';
+    }
+    else{
+        cout << 0 << '\n';
+    }
+    if(a > b){
+        cout << 1 << '\n';
+    }
+    else{
+        cout << 0 << '\n';
+    }
+    if(a <= b){
+        cout << 1 << '\n';
+    }
+    else{
+        cout << 0 << '\n';
+    }
+    if(a < b){
+        cout << 1 << '\n';
+    }
+    else{
+        cout << 0 << '\n';
+    }
+    if(a == b){
+        cout << 1 << '\n';
+    }
+    else{
+        cout << 0 << '\n';
+    }
+    if(a != b){
+        cout << 1 << '\n';
+    }
+    else{
+        cout << 0 << '\n';
+    }
+    return 0;
+}

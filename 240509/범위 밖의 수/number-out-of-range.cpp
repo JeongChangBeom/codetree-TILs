aabd914328f52520e4be8387e4037b6dcfb7a86c
ChangBeom <87409157+JeongChangBeom@users.noreply.github.com>
2024-05-09 18:07:16 +0900

    Title: 범위 밖의 수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240509/범위 밖의 수/number-out-of-range.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+
+    if(a < 10 || a > 20){
+        cout << "yes";
+    }
+    else{
+        cout << "no";
+    }
+    return 0;
+}

9b706a57e91cd99ee626913f7e6e71388b38fed5
ChangBeom <87409157+JeongChangBeom@users.noreply.github.com>
2024-05-09 18:10:16 +0900

    Title: 굉장한 숫자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240509/굉장한 숫자/amazing-number.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    if((n%2!=0 && n%3==0) || (n%2==0 && n%5 == 0)){
+        cout << "true";
+    }
+    else{
+        cout << "false";
+    }
+    return 0;
+}

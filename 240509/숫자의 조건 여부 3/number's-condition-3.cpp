ed8641fec4be1f7c21f46789e2c6a6eff3b7f373
ChangBeom <87409157+JeongChangBeom@users.noreply.github.com>
2024-05-09 18:08:16 +0900

    Title: 숫자의 조건 여부 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240509/숫자의 조건 여부 3/number's-condition-3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+
+    if(a % 13 == 0 || a % 19 == 0){
+        cout << "True";
+    }
+    else{
+        cout << "False";
+    }
+    return 0;
+}
